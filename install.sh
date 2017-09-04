3e63fc04b9da5771ff64dc6cac86ea5b00516745
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2017-09-04 17:18:39 +0300

    Install.sh: Attach it to bash_profilewq

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ echo "# ###" >> $targetFile
 echo "PATH=\$PATH:\"`pwd`\"" >> $targetFile
 echo >> $targetFile
 
+
+echo >> ~/.bash_profile
+echo "# FuturistLabs Env" >> ~/.bash_profile
+echo 'source ~/.bash_flabs' >> ~/.bash_profile
+echo >> ~/.bash_profile
+
