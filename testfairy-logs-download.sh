b24ef3296cc0f766a97bdcd81378aa809e6d5ee7
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2018-03-20 23:24:05 +0200

    Changed test fairy logs downloader

--- testfairy-logs-download.sh
+++ testfairy-logs-download.sh
@@ -2,11 +2,12 @@
 
 function print_help() {
   echo
-  echo "Usage: $0 app build last-session-id"
-  echo "  app             - app id (the value after /project/)"
-  echo "  build           - build id (the value after /builds/)"
-  echo "  last-session-id - the largest session number for this build"
-  echo "  help            - print this help"
+  echo "Usage: $0 app build last-session-id start-session-id"
+  echo "  app              - app id (the value after /project/)"
+  echo "  build            - build id (the value after /builds/)"
+  echo "  last-session-id  - the largest session number for this build"
+  echo "  start-session-id - the lowest session number for this build to fetch. Default is 1"
+  echo "  help             - print this help"
   echo
   echo "In order to get config parameters, go to the page with list of session you want to download and get the URL."
   echo "e.g. https://app.testfairy.com/projects/16629-mytestapp/builds/3345302/sessions"
@@ -19,7 +20,8 @@ function download() {
   app=$1
   build=$2
   sessionId=$3
-  curl "https://app.testfairy.com/projects/$app/builds/$build/sessions/$sessionId/save-logcat" -H 'Cookie: SpryMedia_DataTables_latest-sessions_5302334=%7B%22iCreate%22%3A1491939624535%2C%22iStart%22%3A60%2C%22iEnd%22%3A70%2C%22iLength%22%3A10%2C%22aaSorting%22%3A%5B%5B0%2C%22desc%22%5D%5D%2C%22oSearch%22%3A%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%22aoSearchCols%22%3A%5B%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%2C%7B%22bCaseInsensitive%22%3Atrue%2C%22sSearch%22%3A%22%22%2C%22bRegex%22%3Afalse%2C%22bSmart%22%3Atrue%7D%5D%2C%22abVisCols%22%3A%5Btrue%2Ctrue%2Ctrue%2Ctrue%2Ctrue%2Ctrue%2Ctrue%2Ctrue%5D%7D; q=1487668970; fs_uid=www.fullstory.com`2136V`5159532518965248:5665117697998848`53902`; l=NTM5MDItMTQ5MTkyMDcwOC0zOTRjODU2NWJiYjI1NDcwZTEzYmQwYTYzZWE4NGY2Y2QwZGYxMzAy; oribi_user_guid=346594cf-27e6-4667-24d6-ad315d048656; G_ENABLED_IDPS=google; _ga=GA1.2.2116701706.1487668966; intercom-session-y7zoicwn=c3JyU2pvVCswMWxyK2VyTGcrektnYXFGWjcvMkxFTEdTY3I5ejJKWDJ6N0g2eW5UdDVQK25HdHRSQk1WaWEzcy0tcGMwN2wrdFAzSzZxVXVPWnV0M2Nsdz09--8cb53677446fdaa800c81575058b6f04f12ddd24' > "session-${sessionId}.log";
+  cookieValue='Cookie: q=1498425981; fs_uid=fullstory.com`2136V`6398380880166912:5629499534213120`53902`; oribi_user_guid=fdf09b44-ca3a-9fbb-53d6-c187c3e11fbb; _ga=GA1.2.2012864859.1498425954; intercom-lou-y7zoicwn=1; G_ENABLED_IDPS=google; l=NTM5MDItMTUyMDYwMjU2OC1kYmZjNzc3ZWNjYjgwYzc1MjUwZjkzNDFmZGQ0ZWRiMzNkY2MyNDUy; _gid=GA1.2.174398859.1521208227; _gat=1; intercom-session-y7zoicwn=eG1lOENTbjdkUkxZdmZpUFROWXBlVW92amV4eWV0YlgwMk5Bd0VOaVJjNVJVS3JKejhOWXJtTEl1OWR4QTZWKy0tWkRBeFp2TkNVckFyZUJaWnVZRldwQT09--afed5585a4d987e3f0898904f5a599fdcbd27773'
+  curl "https://app.testfairy.com/projects/$app/builds/$build/sessions/$sessionId/save-logcat" -H "$cookieValue" > "session-${sessionId}.log";
 }
 
 #
@@ -28,6 +30,7 @@ function download() {
 app=$1
 build=$2
 lastSessionId=$3
+startSessionId=$4
 
 if [ -z "$app" ]; then
   echo
@@ -50,7 +53,11 @@ if [ -z "$lastSessionId" ]; then
   exit
 fi
 
-for ((I=1; I<=$lastSessionId; I++))
+if [ -z "$startSessionId" ]; then
+  startSessionId=1
+fi
+
+for ((I=$lastSessionId; I>=$startSessionId; I--))
 do
 	download "$app" "$build" $I
 done
