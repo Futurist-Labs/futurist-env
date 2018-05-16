bf27186ba6e5a424d0b6a68244423cf0cd29b7b1
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2018-05-16 15:49:52 +0300

    Add test-coverage script

---
+++ test-coverage-repositories.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function sectionHeader() {
+  echo
+  echo
+  echo
+  echo "# $1"
+  echo "==========================================================="
+}
+
+egrep -lir --include=*Repository.java "public interface" ./src/main/ | xargs grep '(' | sed -E 's|.*/(.*)\.java.* (.*)\(.*|\1::\2|' | sort | uniq > repos.tmp
+find ./src/test/ -name '*RepositoryTest.java' | xargs grep -A 1 'Test' | grep  "public" | grep -v "public class " | sed -E 's|.*/([^/]*)Test.java.*void ([^_\(]*).*|\1::\2|' | sort | uniq > tests.tmp
+
+sectionHeader "COVERED"
+comm -12 repos.tmp tests.tmp
+
+sectionHeader "TEST WITH WRONG NAMES"
+comm -13 repos.tmp tests.tmp
+
+sectionHeader "NOT COVERED"
+comm -23 repos.tmp tests.tmp
+
+sectionHeader "STATS"
+echo "# COVERED: " `comm -12 repos.tmp tests.tmp | wc -l`
+echo "# TEST WITH WRONG NAMES: " `comm -13 repos.tmp tests.tmp | wc -l`
+echo "# NOT COVERED: " `comm -23 repos.tmp tests.tmp | wc -l`
+echo
+
+rm repos.tmp
+rm tests.tmp
